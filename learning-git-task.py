1cc357b11a3a164ab18eb652962fe04c0c5f6b05
Mateusz Gawroński <gawronski.m91@gmail.com>
2022-09-23 10:19:17 +0200

    change text in first line

---
+++ learning-git-task.py
@@ -0,0 +1,24 @@
+print("Lista zakupów na dzisiaj")
+print()
+
+shopping_dict = {
+    "piekarnia" : ['chleb', 'bułki', 'pączek'],
+    "warzywniak" : ['marchew', 'seler', 'rukola'],
+}
+
+sum = 0
+
+for sklep, produkty in shopping_dict.items():
+    print("Ide do " + sklep.title() + ", kupuję tu następujące rzeczy: "  )
+    for produkt in produkty:
+        print("\t- " + produkt.title())
+        sum += 1
+
+print()
+print(f"W sumie kupuję {sum} produktów.")
+
+
+
+
+
+
